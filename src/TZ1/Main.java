7abcda4c78fa0e28cea5a540daf1b6db85efb192
Fossa25 <topkodanil@mail.com>
2026-07-05 23:18:34 +0500

    Game2

---
+++ src/TZ1/Main.java
@@ -0,0 +1,151 @@
+package TZ1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        do {
+            System.out.println("-".repeat(70));
+            System.out.println("Начать новую игру?" +
+                    "\n" + "1  - Да,конечно! " +
+                    "\n" + "0  - Не, я пошёл.");
+            System.out.println("Ожидаю Вашего ответа");
+            int chois = getInput().nextInt();
+            System.out.println("-".repeat(70));
+            switch (chois) {
+                case 1 -> {
+                    System.out.println("Эта битва будет легендарна!");
+                    setboard();
+                }
+                case 0 -> {
+                    System.out.println("Давай,Дасвиданья!");
+                    return;
+                }
+                default -> System.out.println("Введен не существующий вариант!");
+            }
+
+        } while (true);
+
+    }
+
+    public static Scanner getInput() {
+        return new Scanner(System.in);
+    }
+    public static void setboard() {
+        String[][]board= setStartboard();
+        String ilementPerson;
+        String ilementAI;
+        if((Math.random() * 10)%2==0){
+            ilementPerson="X";
+            ilementAI="0";
+            System.out.println("Вы первый!");
+        }else {
+            ilementPerson="0";
+            ilementAI="X";
+            System.out.println("Первый AI");
+        }
+        do {
+            if (ilementAI.equals("X")){
+            setNewBord(board,ilementAI);}
+            else { setIlement(board, "Person", ilementPerson);}
+            getBord(board);
+            System.out.println("-".repeat(50));
+            if (ilementAI.equals("X")){
+                setIlement(board, "Person", ilementPerson);}
+            else { setNewBord(board,ilementAI);}
+            getBord(board);
+            System.out.println("-".repeat(50));
+        } while (checWin(board,ilementAI) && checWin(board,ilementPerson));
+        if(!checWin(board,ilementAI)){
+            System.out.println("Победил AI! \n Нам срочно нужна Сара Конер!");
+        }else {
+            System.out.println("Победа за тобой! \n Уть, держи медальку");
+        }
+    }
+
+    public static int randomNumber() {
+        int t;
+        do {
+            t = (int) ((Math.random() * 10));
+        }
+        while (t > 2);
+        return t;
+    }
+
+    public static void getBord(String[][] board) {
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                System.out.print((board[row][col] + "\t"));
+            }
+            System.out.println();
+        }
+    }
+
+    public static String[][] setIlement(String[][] board, String who, String ilement) {
+
+        int r, c;
+        if (who.equals("AI")) {
+            r = randomNumber();
+            c = randomNumber();
+        } else {
+            System.out.println("Введите строчку от 0 до 2");
+            r = getInput().nextInt();
+            System.out.println("Введите столб от 0 до 2");
+            c = getInput().nextInt();
+        }
+        if (board[r][c].equals("-")) {
+
+            board[r][c] = ilement;
+            return board;
+
+        } else {
+            if (who.equals("Person")) {
+                System.out.println("В этом месте уже есть значение");
+            }
+            setIlement(board, who, ilement);
+
+        }
+        return board;
+    }
+
+    public static String[][] setNewBord(String[][] board,String ilement) {
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                if (board[row][col].equals("-")) {
+                    setIlement(board, "AI", ilement);
+                    return board;
+                }
+            }
+        }
+        return board;
+    }
+
+    public static boolean checWin(String[][] board, String ilement) {
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0].equals(ilement) && board[i][1].equals(ilement) && board[i][2].equals(ilement)) {
+                return false;
+            } else if (board[0][i].equals(ilement) && board[1][i].equals(ilement) && board[2][i].equals(ilement)) {
+                return false;
+            } else if (board[0][0].equals(ilement) && board[1][1].equals(ilement) && board[2][2].equals(ilement)) {
+                return false;
+            } else if (board[0][2].equals(ilement) && board[1][1].equals(ilement) && board[2][0].equals(ilement)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static String[][] setStartboard() {
+        String[][] board = new String[3][3];
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[row].length; col++) {
+                board[row][col] = "-";
+                System.out.print((board[row][col] + "\t"));
+            }
+            System.out.println();
+        }
+        System.out.println("-".repeat(50));
+        return board;
+    }
+}
