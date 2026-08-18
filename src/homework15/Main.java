8651c4b2349b5e7c70ba48580efb49e638138a6d
Fossa25 <topkodanil@mail.com>
2026-08-18 10:41:03 +0500

    Homework15

--- src/homework15/Main.java
+++ src/homework15/Main.java
@@ -1,8 +1,14 @@
 package homework15;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends SetStringUser {
     public static void main(String[] args) {
        // getNumbers.getNumbersUser();
+       // goZoo();
+        getStudentsList();
+    }
+    public static void goZoo(){
         do {
             System.out.println("-".repeat(70));
             System.out.println(GREEN+"Добрый день, приветствую вас в зоопарке! "+
@@ -14,11 +20,11 @@ public class Main extends SetStringUser {
             switch (getInt()) {
                 case 1 -> {
                     System.out.println("Кого к нам привезли?");
-                   Animals.setAnimal(getInput());
+                    Animals.setAnimal(getInput());
                 }
                 case 2 ->{
 
-                   Animals.deleteAnimal();
+                    Animals.deleteAnimal();
                 }
                 case 0 -> {return;}
                 default -> {
@@ -27,5 +33,19 @@ public class Main extends SetStringUser {
                 }
             }
         }while (true);
+    }
+    public static void getStudentsList(){
+        List<Students> studentsArrayList= new ArrayList<>(List.of(
+                new Students("Ivan","RPM",2,5,4,4),
+                new Students("Asa","PRM",3,5,3,3),
+                new Students("Egor","RPM",5,3,3,2),
+                new Students("Natasha","RPM",2,2,2,4),
+                new Students("Alex","PRM",3,2,3,3)
+        ));
+        System.out.println("Какой курс будем искать?");
+        Students.printStudents(studentsArrayList,SetStringUser.getInt());
+        studentsArrayList= Students.deleteStudents(studentsArrayList);
+        System.out.println("Какой курс будем искать?");
+        Students.printStudents(studentsArrayList,SetStringUser.getInt());
     }
 }
--- src/homework15/Students.java
+++ src/homework15/Students.java
@@ -1,41 +1,65 @@
 package homework15;
 
-public class Students {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Students extends SetStringUser {
+    private final String name;
+    private final String group;
+    private int well;
+    private final int gradeRussia;
+    private final int gradeEnglish;
+    private final int gradeJava;
+    public Students(String name, String group, int well, int gradeRussia, int gradeEnglish, int gradeJava) {
+        this.name = name;
+        this.group = group;
+        this.well = well;
+        this.gradeRussia = gradeRussia;
+        this.gradeEnglish = gradeEnglish;
+        this.gradeJava = gradeJava;
+    }
+    public static List<Students> deleteStudents( List<Students> studentsList){
+        System.out.println("-".repeat(50));
+        System.out.println(GREEN+"Все студенты  \n"+studentsList+RESET);
+        System.out.println("-".repeat(50));
+        List<Students> newList = new ArrayList<>();
+        for (Students qrade : studentsList){
+            if(((qrade.gradeEnglish+qrade.gradeRussia+qrade.gradeJava)/3)>=3){
+               qrade.well = qrade.well + 1;
+                newList.add(qrade);
+                System.out.println(BLUE+"Студент " +qrade.name + " переводиться на новый курс "  +qrade.well+RESET);
+            }
+        }
+        studentsList=newList;
+        System.out.println("-".repeat(50));
+        System.out.println(GREEN+"После отчисления : \n"+studentsList+RESET);
+        System.out.println("-".repeat(50));
+        return studentsList;
+    }
+    public static void printStudents(List<Students> studentsList, int course) {
+        boolean found = false;
+        for (Students student : studentsList) {
+            if (student.well == course) {
+                if (!found) {
+                    System.out.println(YELLOW + "Список студентов обучающихся на " + course + " курсе:" + RESET);
+                    found = true;
+                }
+                System.out.println(GREEN + student.name + RESET);
+            }
+        }
+        if (!found) {
+            System.out.println(RED + "На этом курсе никто не обучается" + RESET);
+        }
+    }
+    @Override
+    public String toString() {
+        return YELLOW+"\n" +
+                " name : " + name +
+                ", group : " + group +
+                ", well : " + well +
+                ", gradeEnglish = " + gradeEnglish +
+                ", gradeRussia = " + gradeRussia +
+                ", gradeJava = " + gradeJava +
+                RESET;
+    }
 }
-/*
-Создать класс Student, содержащий следующие характеристики – имя, группа, курс,
-оценки по предметам. Создать коллекцию, содержащую объекты класса Student.
-        Написать метод, который удаляет студентов со средним баллом <3. Если средний
-балл>=3, студент переводится на следующий курс. Дополнительно написать метод
-printStudents(List<Student> students, int course), который получает список студентов и
-номер курса. А также печатает на консоль имена тех студентов из списка, которые
-обучаются на данном курсе.
-
-
-
-Домашнее задание
-Задача 1:
-На вход поступает массив строк, верните Map<String, Boolean>, где каждая отдельная
-строка является ключом, и ее значение равно true, если эта строка встречается в массиве
-2 или более раз. Пример:
-wordMultiple(["a", "b", "a", "c", "b"]) → {"a": true, "b": true, "c": false}
-wordMultiple(["c", "b", "a"]) → {"a": false, "b": false, "c": false}
-wordMultiple(["c", "c", "c", "c"]) → {"c": true}
-Задача 2:
-
-На вход поступает массив непустых строк, создайте и верните Map<String,
-String> следующим образом: для каждой строки добавьте ее первый символ в
-качестве ключа с последним символом в качестве значения. Пример:
-pairs(["code", "bug"]) → {"b": "g", "c": "e"}
-pairs(["man", "moon", "main"]) → {"m": "n"}
-pairs(["man", "moon", "good", "night"]) → {"g": "d", "m": "n", "n": "t"}
-Задача *:
-Задана строка, которая может иметь внутри себя скобки. Скобкой считается
-любой из следующих символов — «(«, «)», «[«, «]», «{«, «}». Проверить
-сбалансированность расстановки скобок в этой строке. Набор скобок считается
-сбалансированной парой, если открывающая скобка "(", "[" и "{" стоит слева от
-соответствующей закрывающей скобки ")", "]" и "} ", соответственно. Строка,
-содержащая пары скобок, не сбалансирована, если набор заключенных в нее
-скобок не совпадает .
-Например, если ввод «{[(])}», пара квадратных скобок «[]» заключает в себя
- */
